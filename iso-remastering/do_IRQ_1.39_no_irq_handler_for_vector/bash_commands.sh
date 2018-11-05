c021da4c46859630c29b28c0b997887dd65fda1c
Zoran Stojsavljevic <zoran.stojsavljevic@gmail.com>
2018-11-05 13:31:49 +0100

    bash_commands.sh - some useful commands to test do_IRQ: 1.39 No…irq handler for vector issue!

--- iso-remastering/do_IRQ_1.39_no_irq_handler_for_vector/bash_commands.sh
+++ iso-remastering/do_IRQ_1.39_no_irq_handler_for_vector/bash_commands.sh
@@ -1,5 +1,14 @@
+## bash_commands.sh - some useful commands to test do_IRQ: 1.39 No…irq handler for vector issue!
+
 ## Command to find all active IOAPIC interrupts using /sys file system
 find . -name irq -print -exec cat {} \;
+
+## Browse INT (IO-APIC and PCI-MSI domains)
+cat /proc/interrupts
+
+## Find the mapings of the two IOAPIC interrupts to the vector number
+cd /sys/kernel/debug/irq/irqs
+/sys/kernel/debug/irq/irqs# grep 39 *
 
 ## Command to start Graphical Desktop Manager (GDM)
 systemctl start getty@tty7
